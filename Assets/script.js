ef90e89c9cbb6cee63bec72f5d5ef423c33fb1c6
loganeworth <loganeworth@gmail.com>
2023-01-16 19:37:57 -0600

    finished savebtn function

--- Assets/script.js
+++ Assets/script.js
@@ -63,6 +63,17 @@ $(document).ready(function () {
   
   // $('hour-8', 'description').val.localStorage.getItem('hour-8', 'description')
 
+  $('#hour-8 .description').val(localStorage.getItem('hour-8'))
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'))
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'))
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'))
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'))
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'))
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'))
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'))
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'))
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'))
+
   // $('.time-block').each(function() {
   //   let storedText = localStorage.getItem($(this).attr('id'))
   //   console.log(storedText)
@@ -71,16 +82,6 @@ $(document).ready(function () {
   //   // $('#hour-11 .description').text(storedText)
   // })
 
-$('#hour-8 .description').val(localStorage.getItem('hour-8'))
-$('#hour-9 .description').val(localStorage.getItem('hour-9'))
-$('#hour-10 .description').val(localStorage.getItem('hour-10'))
-$('#hour-11 .description').val(localStorage.getItem('hour-11'))
-$('#hour-12 .description').val(localStorage.getItem('hour-12'))
-$('#hour-13 .description').val(localStorage.getItem('hour-13'))
-$('#hour-14 .description').val(localStorage.getItem('hour-14'))
-$('#hour-15 .description').val(localStorage.getItem('hour-15'))
-$('#hour-16 .description').val(localStorage.getItem('hour-16'))
-$('#hour-17 .description').val(localStorage.getItem('hour-17'))
 
   // The below line is retrieving the stored in item from local storage
   // localStorage.getItem('hour-8', 'description')
